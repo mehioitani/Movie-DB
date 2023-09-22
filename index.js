12bc93c4b6e87a15e4fdcc9779b3ed1297aec881
mehioitani <muhieddineitani04@gmail.com>
2023-09-23 01:16:11 +0300

    Step 3

--- index.js
+++ index.js
@@ -6,4 +6,27 @@ app.get('/', (req, res) => {
   res.send('ok')
 })
 
+app.get('/test', (req, res) => {
+    const response = {
+      status: 200,
+      message: 'ok'
+    };
+  
+    res.send(response);
+  });
+
+  app.get('/time', (req, res) => {
+    const timeNow = new Date();
+    const hours = timeNow.getHours();
+    const minutes = timeNow.getMinutes();
+    const seconds = timeNow.getSeconds();
+    const time = `${hours}:${minutes}:${seconds}`;
+    const response = {
+      status: 200,
+      message: time
+    };
+  
+    res.send(response);
+  });
+
 app.listen(port);
